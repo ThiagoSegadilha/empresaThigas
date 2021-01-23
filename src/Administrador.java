819da219a31bdbb6a67ae12895348bd8a8e2c1f8
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2021-01-23 17:50:55 -0300

    criacao de um atenticador para evitar repeticao no codigo

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,23 +1,23 @@
 public class Administrador extends Funcionario implements Autenticavel {
 
-    private int senha;
+    private Autenticador autenticador;
 
-    public double getBonificacao() {
-        System.out.println("Bonificacao do Gerente");
-        return super.getSalario() * 0.35;
+    public Administrador () {
+        this.autenticador = new Autenticador();
     }
 
     @Override
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autenticacao(int senha) {
-        if (this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.autenticador.autenticacao(senha);
+    }
+
+    public double getBonificacao() {
+        System.out.println("Bonificacao do Gerente");
+        return super.getSalario() * 0.35;
     }
 }
--- src/Gerente.java
+++ src/Gerente.java
@@ -1,19 +1,19 @@
 public class Gerente extends Funcionario implements Autenticavel{
 
-    private int senha;
+    private Autenticador autenticador;
+
+    public Gerente () {
+        this.autenticador = new Autenticador();
+    }
 
     @Override
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autenticacao(int senha) {
-        if (this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.autenticador.autenticacao(senha);
     }
 
     public double getBonificacao() {
