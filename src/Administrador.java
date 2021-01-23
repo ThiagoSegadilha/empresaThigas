a3ed1f90f82ae8215e1576baf35fbc37f9e8e012
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2021-01-23 17:28:10 -0300

    fortalecendo o uso de interface

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,7 +1,23 @@
-public class Administrador extends Funcionario {
+public class Administrador extends Funcionario implements Autenticavel {
+
+    private int senha;
 
     public double getBonificacao() {
         System.out.println("Bonificacao do Gerente");
         return super.getSalario() * 0.35;
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autenticacao(int senha) {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
